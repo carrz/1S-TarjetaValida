2c8e9ddd9fa9de034d267d6c32545d1a3f83a5f5
carrz <carla.zumaran@gmail.com>
2018-01-27 23:01:20 -0300

    Arreglo de estilos para google

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
 
-function validNumbers(){
-var cardNumber = "";
-  do{ 
-    cardNumber= prompt("Ingrese los números de su tarjeta de crédito a validar");
-    if (cardNumber !== ""){
-    	isValidCard(cardNumber);
-    }else{
-    	alert("Ingrese el número de su tarjeta de crédito");
-    }   
-  }while (cardNumber === "");
+function validNumbers() {
+	var cardNumber = '';
+	do {
+		cardNumber = prompt('Ingrese los números de su tarjeta de crédito a validar');
+		if (cardNumber !== '') {
+			isValidCard(cardNumber);
+		} else {
+			alert('Ingrese el número de su tarjeta de crédito');
+		}
+	} while (cardNumber === '');
 }
 
-function isValidCard(cardNumber){
-var cardNumberInv=[];
-var result = 0;
-for (var i=0; i<cardNumber.length; i++){
-  cardNumberInv.unshift(parseInt(cardNumber[i]));
-}
-cardNumberInv.unshift("N");
-
-for (var j=1; j<cardNumberInv.length; j++){
-	var resultOdd= 0;
-    var resultPair10=0;
-    var resultPair=0;
-    result=resultPair+resultOdd+resultPair10;
-	if(j%2!==0){
-		result += cardNumberInv[j];
+function isValidCard(cardNumber) {
+	var cardNumberInv = [];
+	var result = 0;
+	for (var i = 0; i < cardNumber.length; i++) {
+		cardNumberInv.unshift(parseInt(cardNumber[i]));
 	}
-	else if(j%2===0){
-		if(cardNumberInv[j]*2>10){
-			(cardNumberInv[j]-10)+1;
+
+	for (var j = 0; j < cardNumberInv.length; j++) {
+		if (j % 2 === 0) {
 			result += cardNumberInv[j];
-		}
-		else {
-			result += (cardNumberInv[j]*2);
+		}	else {
+			if (cardNumberInv[j] * 2 > 10) {
+				result += (cardNumberInv[j] * 2 - 10) + 1;
+			}	else {
+				result += (cardNumberInv[j] * 2);
+			}
 		}
 	}
-}
-if(result%10===0){
-	document.write("Felicidades, su tarjeta es válida");
-}
-else {
-	document.write("Lo sentimos, su tarjeta no es válida");
-}
+	if (result % 10 === 0) {
+		alert('Felicidades, su tarjeta es válida');
+	} else {
+		alert('Lo sentimos, su tarjeta no es válida');
+	}
 }
 validNumbers();
